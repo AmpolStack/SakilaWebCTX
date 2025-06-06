197adda03bf97280485d74590dff347ae8b5843a
Amapola <htrsanbl@gmail.com>
2025-06-05 22:12:05 -0500

    feat: Implements authentication in login view

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, model, signal } from '@angular/core';
+import { AuthHandlerService } from '../auth-handler.service';
+import { FormsModule } from '@angular/forms';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
 
+  public emailAddress = signal<string>('');
+  public password = signal<string>('');
+
+  constructor(private authService: AuthHandlerService) {
+  }
+
+  login(username: string, password: string) {
+    this.authService.ObtainAuthentication(username, password).subscribe({
+      next: (response) => {
+        if (response.success) {
+          console.log(response.data);
+          // Aquí puedes guardar el token o redirigir
+        } else {
+          console.error('Error en login:', response.error);
+        }
+      },
+      error: (error) => {
+        console.error('Error en la petición:', error);
+      }
+    });
+  }
 }
