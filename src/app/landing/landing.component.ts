eff33cad6ad437c15f67e307e59fc812b5570e7c
Amapola <htrsanbl@gmail.com>
2025-06-07 01:34:01 -0500

    feat: Include new templates and styles for Landing Component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { API_URL } from '../app.config';
 
 @Component({
   selector: 'app-landing',
@@ -8,5 +9,6 @@ import { RouterModule } from '@angular/router';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
-
+  public API_URL : string = inject(API_URL)
+  public FRONT_URL : string = 'http://localhost:4200'
 }
