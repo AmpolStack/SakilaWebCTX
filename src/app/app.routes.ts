8f43693eb0dfcecd4a4325877e3b225f3719c361
Amapola <htrsanbl@gmail.com>
2025-06-06 23:30:31 -0500

    feat: Includes landing component, tags, css, and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
+    { path: 'home', component: LandingComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent},
-    { path: '**', redirectTo: '' }
+    { path: '**', component: LandingComponent}
 ];
