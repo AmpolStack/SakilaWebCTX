d387a396e197d535266c95f377f714ab63adc72c
Amapola <htrsanbl@gmail.com>
2025-06-07 17:41:17 -0500

    feat: The environment is adapted to implement Popup component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PopUpContent, PopUpType, PopUpWindowComponent } from './pop-up-window/pop-up-window.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PopUpWindowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sakila_pjt';
+  
+  public activePopUp = signal<boolean>(false);
+
+  defaultContent : PopUpContent = {
+    reason: 'PopUpWindow Active!',
+    description: 'It is just a tests for popUp, example: error'
+  };
+
+  defaulType = PopUpType.Info;
 }
