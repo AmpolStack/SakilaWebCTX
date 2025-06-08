d98ce32160563ede917a35872004d0caa56d9216
Amapola <htrsanbl@gmail.com>
2025-06-07 23:34:51 -0500

    feat: register the last 'main' component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     { path: 'home', component: LandingComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent},
+    { path: 'main', component: MainComponent},
     { path: '**', component: LandingComponent}
 ];
