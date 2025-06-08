6b635d4629d789ec216a3dc31b2285a049e22c58
Amapola <htrsanbl@gmail.com>
2025-06-08 18:15:21 -0500

    refactor: Update the workflow, and use the carousel compoment dinamically

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { LandingComponent } from '../landing/landing.component';
+import { CarouselComponent, carouselProps, movie } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  public defaultMovies : movie[] = [
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+  ]
 
+  public defaultCarrousels : carouselProps[] = [
+    {title: 'Movie Category', movies: this.defaultMovies},
+    {title: 'Movie Category', movies: this.defaultMovies},
+    {title: 'Movie Category', movies: this.defaultMovies},
+    {title: 'Movie Category', movies: this.defaultMovies},
+    {title: 'Movie Category', movies: this.defaultMovies}
+  ]
 }
