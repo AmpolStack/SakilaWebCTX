844efbb68f801da51523796e828d36db24d78d6a
Amapola <htrsanbl@gmail.com>
2025-06-08 18:16:13 -0500

    feat: Register the movie component and update any css properties

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { MainComponent } from './main/main.component';
+import { MovieComponent } from './movie/movie.component';
 
 export const routes: Routes = [
-    { path: 'home', component: LandingComponent},
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'landing', component: LandingComponent},
+    { path: '', redirectTo: 'landing', pathMatch: 'full' },
     { path: 'login', component: LoginComponent},
     { path: 'main', component: MainComponent},
+    { path: 'movie', component: MovieComponent},
     { path: '**', component: LandingComponent}
 ];
