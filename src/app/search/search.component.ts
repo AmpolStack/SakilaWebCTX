c4874bba30e1f3160a3c83aa75afe0af88ad0ccf
Amapola <htrsanbl@gmail.com>
2025-06-08 23:58:00 -0500

    feat: Create the Search Component

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CarouselComponent, carouselProps, movie } from '../carousel/carousel.component';
+
+@Component({
+  selector: 'app-search',
+  imports: [CarouselComponent],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+public defaultMovies : movie[] = [
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+    { id: 0, name: 'Movie Name', link: ''},
+  ]
+
+  public defaultCarrousels : carouselProps ={
+    title: 'Similar Movies', 
+    movies: this.defaultMovies
+  }
+}
