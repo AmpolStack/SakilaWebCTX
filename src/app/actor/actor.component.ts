e9262874286cd7eaf2243790a7e61d61c49f4822
Amapola <htrsanbl@gmail.com>
2025-06-09 16:57:40 -0500

    fix: Temporary problems are resolved by the track index

--- src/app/actor/actor.component.ts
+++ src/app/actor/actor.component.ts
@@ -9,16 +9,16 @@ import { CarouselComponent, carouselProps, movie } from '../carousel/carousel.co
 })
 export class ActorComponent {
     public defaultMovies : movie[] = [
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
-      { id: 0, name: 'Movie Name', link: ''},
+      { id: 1, name: 'Movie Name', link: ''},
+      { id: 2, name: 'Movie Name', link: ''},
+      { id: 3, name: 'Movie Name', link: ''},
+      { id: 4, name: 'Movie Name', link: ''},
+      { id: 5, name: 'Movie Name', link: ''},
+      { id: 6, name: 'Movie Name', link: ''},
+      { id: 7, name: 'Movie Name', link: ''},
+      { id: 8, name: 'Movie Name', link: ''},
+      { id: 9, name: 'Movie Name', link: ''},
+      { id: 10, name: 'Movie Name', link: ''},
     ]
   
     public defaultCarrousels : carouselProps= {
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,15 +10,15 @@ import { CarouselComponent, carouselProps, movie } from '../carousel/carousel.co
 export class MainComponent {
   public defaultMovies : movie[] = [
     { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 0, name: 'Movie Name', link: ''},
+    { id: 1, name: 'Movie Name', link: ''},
+    { id: 2, name: 'Movie Name', link: ''},
+    { id: 3, name: 'Movie Name', link: ''},
+    { id: 4, name: 'Movie Name', link: ''},
+    { id: 5, name: 'Movie Name', link: ''},
+    { id: 6, name: 'Movie Name', link: ''},
+    { id: 7, name: 'Movie Name', link: ''},
+    { id: 8, name: 'Movie Name', link: ''},
+    { id: 9, name: 'Movie Name', link: ''},
   ]
 
   public defaultCarrousels : carouselProps[] = [
