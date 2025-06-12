a4278c2b7f6ea8d87debcc8c03bf23f7141a09c7
Amapola <htrsanbl@gmail.com>
2025-06-11 23:55:19 -0500

    feat: Implements logic to use dinamic information

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,28 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject, signal } from '@angular/core';
 import { CarouselComponent, carouselProps, movie } from '../carousel/carousel.component';
+import { ActivatedRoute } from '@angular/router';
+import { DataService, Expensefilm } from '../data.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, TitleCasePipe],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
 export class MovieComponent {
-public defaultMovies : movie[] = [
-    { id: 0, name: 'Movie Name', link: ''},
-    { id: 1, name: 'Movie Name', link: ''},
-    { id: 2, name: 'Movie Name', link: ''},
-    { id: 3, name: 'Movie Name', link: ''},
-    { id: 4, name: 'Movie Name', link: ''},
-    { id: 5, name: 'Movie Name', link: ''},
-    { id: 6, name: 'Movie Name', link: ''},
-    { id: 7, name: 'Movie Name', link: ''},
-    { id: 8, name: 'Movie Name', link: ''},
-    { id: 9, name: 'Movie Name', link: ''},
-  ]
+  private router = inject(ActivatedRoute);
+  private service = inject(DataService);
+  public movie = signal<Expensefilm | null>(null)
 
-  public defaultCarrousels : carouselProps[] = [
-    {title: 'Similar Movies', movies: this.defaultMovies},
-    {title: 'Recommended Movies', movies: this.defaultMovies},
-  ]
+  constructor(){
+    this.router.paramMap.subscribe(params =>{
+      const referenceId = params.get('referenceId');
+      let response = this.service.ObtainFilmByReferenceId(referenceId!)
+        .subscribe({
+          next: (success) =>{
+            this.movie.set(success.data);
+            console.log(success);
+          },
+          error: (error) =>{
+            console.error(error)
+          }
+        })
+    })
+  }
+
+  public defaultMovies : movie[] = [
+      { id: (0).toString(), name: 'Movie Name', link: ''},
+      { id: (1).toString(), name: 'Movie Name', link: ''},
+      { id: (2).toString(), name: 'Movie Name', link: ''},
+      { id: (3).toString(), name: 'Movie Name', link: ''},
+      { id: (4).toString(), name: 'Movie Name', link: ''},
+      { id: (5).toString(), name: 'Movie Name', link: ''},
+      { id: (6).toString(), name: 'Movie Name', link: ''},
+      { id: (7).toString(), name: 'Movie Name', link: ''},
+      { id: (8).toString(), name: 'Movie Name', link: ''},
+      { id: (9).toString(), name: 'Movie Name', link: ''},
+    ]
+
+    public defaultCarrousels : carouselProps[] = [
+      {title: 'Similar Movies', movies: this.defaultMovies},
+      {title: 'Recommended Movies', movies: this.defaultMovies},
+    ]
 }
